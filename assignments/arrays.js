8ad564ec0f834eb9500a451b31e0bbaaad8fad7f
ShaunKolich <employmeinc@gmail.com>
2019-04-16 21:02:44 -0400

    Read the list, gave a var models to equal the car makes, then used the .sort() command, then printed new list

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -340,6 +340,14 @@ console.log();
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
+for (let i = 0; i < inventory.length; i++) {
+    models = inventory[i].car_model;
+    carModels.push(models);
+    carModels.sort();
+
+
+}
+console.log(carModels);
 console.log();
 
 // ==== Challenge 4 ====
